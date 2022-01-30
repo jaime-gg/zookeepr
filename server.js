7e40b9c4de9c9dd3d5be10802f8de854bb314417
jester <jaimebuddy98@gmail.com>
2022-01-29 18:44:50 -0600

    used get, post, and fetch to add html and other pages

--- server.js
+++ server.js
@@ -3,15 +3,17 @@ const path = require('path');
 const express = require('express');
 const { animals } = require('./data/animals');
 
-
+//MIDDLEWERE FUNCTIONS ------------------------------------------------------------------------------- 
 const PORT = process.env.PORT || 3001;
 const app = express();
+app.use(express.static('public'));
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
 
+// FILTER SEARCH FUNCTIONS --------------------------------------------------------------------------
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     // Note that we save the animalsArray as filteredResults here:
@@ -56,6 +58,7 @@ function findById(id, animalsArray) {
   return result;
 };
 
+//CREATE NEW ANIMAL FUNCTIONS -----------------------------------------------------------------------
 function createNewAnimal(body, animalsArray) {
   const animal = body; 
   animalsArray.push(animal)
@@ -83,6 +86,8 @@ function validateAnimal(animal) {
   return true;
 }
 
+
+// GET AND POST METHODS -----------------------------------------------------------------------------
 app.get('/api/animals', (req, res) => {
   let results = animals;
   if (req.query) {
@@ -100,6 +105,23 @@ app.get('/api/animals/:id', (req, res) => {
   }
 });
 
+// GETS INDEX.HTML
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+// GET ANIMALS.HTML
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+// GET ZOOKEEPER.HTML 
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+// IN THE EVENT OF BAD URL, SEND TO INDEX.HTML
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 app.post('/api/animals', (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = animals.length.toString();
@@ -112,6 +134,9 @@ app.post('/api/animals', (req, res) => {
     res.json(animal);
   }
 });
+
+
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
